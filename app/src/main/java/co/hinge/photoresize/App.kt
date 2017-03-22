64c278e439b08139b4a46fa46cb585442ef8a29f
Jason Pearson <jason.d.pearson@gmail.com>
2017-03-22 17:25:34 -0400

    register glide

--- app/src/main/java/co/hinge/photoresize/App.kt
+++ app/src/main/java/co/hinge/photoresize/App.kt
@@ -1,13 +1,22 @@
 package co.hinge.photoresize
 
 import android.app.Application
+import okhttp3.OkHttpClient
 import timber.log.Timber
 
 class App : Application() {
+
+    lateinit var okHttp: OkHttpClient
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         Timber.i("App Started")
+
+        okHttp = OkHttpClient.Builder()
+                .build()
+
+        Timber.i("Register glide")
+        GlideProvider.registerComponents(this, okHttp)
     }
 }

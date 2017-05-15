8846d8bba65af3d0497d589322b560b421a71305
Jason Pearson <jason.d.pearson@gmail.com>
2017-05-15 14:52:02 -0400

    should use view pager that implements wrap content

---
+++ app/src/main/java/io/kaeawc/photoresize/select/WrapContentHeightViewPager.kt
@@ -0,0 +1,27 @@
+package io.kaeawc.photoresize.select
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+
+class WrapContentHeightViewPager : ViewPager {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        var heightMeasureSpec = heightMeasureSpec
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        var height = 0
+        for (i in 0..childCount - 1) {
+            val child = getChildAt(i)
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
+            val h = child.measuredHeight
+            if (h > height) height = h
+        }
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+}
